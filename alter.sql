33ca6e7e9be2785cf763f8f6fa30684b573b03c4
Imranhasanrimon <imranhasanrimon5@gmail.com>
2025-05-19 12:54:14 +0600

    exploring some alter queries like how to change table column name, add new column, change data type etc

--- alter.sql
+++ alter.sql
@@ -1,2 +1,11 @@
 -- Active: 1747586967298@@127.0.0.1@5432@ph
 SELECT * FROM person2;
+
+ALTER TABLE person2
+ADD COLUMN email VARCHAR(50) DEFAULT 'default@gamil.com' NOT NULL;
+
+ALTER Table person2 DROP COLUMN email;
+
+ALTER Table person2 RENAME COLUMN age to user_age;
+
+-- INSERT INTO person2 VALUES(13,'mokbul',90,'test@gmail.com')
